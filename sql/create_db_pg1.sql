81bbd20183982cc77391e46bc37fbecaf8b67a24
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-12 06:02:10 +0000

    fix the spell of a column in create database PG1

--- sql/create_db_pg1.sql
+++ sql/create_db_pg1.sql
@@ -1,10 +1,11 @@
+drop database pg1;
 create database pg1;
 CREATE TABLE pg1.pg(
    id INT AUTO_INCREMENT PRIMARY KEY,
    has_food bool,
    has_laundary bool,
    has_airconditioner bool,
-   Level_of_furnitioning int,
+   Level_of_furnshing int,
    has_tv bool,
    has_internet bool,
    has_waterheater bool,
