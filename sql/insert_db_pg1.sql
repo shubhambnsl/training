81173795bddbc1403941ebff6861154ea888c546
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-12 06:12:46 +0000

    insert more values in database PG

--- sql/insert_db_pg1.sql
+++ sql/insert_db_pg1.sql
@@ -1,2 +1,8 @@
 insert into pg(name,address,has_food,has_laundary,has_airconditioner,Level_of_furnshing,has_tv,has_internet,has_waterheater,rent,distance_from_work)
 VALUES("abc_pg","sector-28",true,false,true,4,false,true,false,700,2);
+insert into pg(name,address,has_food,has_laundary,has_airconditioner,Level_of_furnshing,has_tv,has_internet,has_waterheater,rent,distance_from_work)
+VALUES("xyz_pg","sector-27",false,true,false,6,true,false,true,800,3);
+insert into pg(name,address,has_food,has_laundary,has_airconditioner,Level_of_furnshing,has_tv,has_internet,has_waterheater,rent,distance_from_work)
+VALUES("pqr_pg","sector-26",true,true,false,3,false,true,true,500,1);
+insert into pg(name,address,has_food,has_laundary,has_airconditioner,Level_of_furnshing,has_tv,has_internet,has_waterheater,rent,distance_from_work)
+VALUES("qwe_pg","sector-25",false,false,true,7,true,false,false,200,6);
