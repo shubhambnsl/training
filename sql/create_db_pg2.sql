11c621115df0a8302cfcf990dcd7549bffbffbb5
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-12 08:06:49 +0000

    fix the error by simply removing ','

--- sql/create_db_pg2.sql
+++ sql/create_db_pg2.sql
@@ -4,7 +4,7 @@
 drop database pg2;
 
 -- Then create the database
-create database pg2;
+create database pg12;
 
 -- Create the table that stores the Paying Guest Table Data
 create table pg2.pg
@@ -12,20 +12,20 @@ create table pg2.pg
 id INT AUTO_INCREMENT PRIMARY KEY,
 name char(20),
 addr varchar(30),
- created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+ created_on timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Create the table that stores the Feature of the Paying Guest table, User can store as many as feature, a PG has
 -- but the name of feature and the value can't have more then 100 character. 
 -- this table refrences pg table using column pg_id
+-- created_by = userid or username (db or app)
+-- last_update_by = userid or username (db or app)
 create table pg2.features
 (
  id INT AUTO_INCREMENT PRIMARY KEY,
  feature_name varchar(100),
  value varchar(100),
  created_on timestamp DEFAULT CURRENT_TIMESTAMP,
- -- created_by = userid or username (db or app)
- -- last_update_by = userid or username (db or app)
  pg_id INT,
  FOREIGN KEY (pg_id) 
         REFERENCES pg(id)
