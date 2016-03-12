8f8ce2d015b0c2f12f6b4822aee1ce4f0b145604
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-12 06:07:06 +0000

    fix spell in insertion values in database PG

--- sql/insert_db_pg1.sql
+++ sql/insert_db_pg1.sql
@@ -1,2 +1,2 @@
-insert into pg(name,address,has_food,has_laundary,has_airconditioner,Level_of_furnishing,has_tv,has_internet,has_waterheater,rent,distance_from_work)
+insert into pg(name,address,has_food,has_laundary,has_airconditioner,Level_of_furnshing,has_tv,has_internet,has_waterheater,rent,distance_from_work)
 VALUES("abc_pg","sector-28",true,false,true,4,false,true,false,700,2);
