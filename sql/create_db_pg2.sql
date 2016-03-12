236fc9433db8225752d2cdac321ce73c6bc50313
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-12 07:49:58 +0000

    create a database PG2 that has two tables, one is pg that contain the pg details like name,addr and id and another one is features that define the different features of pg, before creating this table, first i drop the database if exists

---
+++ sql/create_db_pg2.sql
@@ -0,0 +1,40 @@
+-- First drop the database 
+-- if database doesn't exist it will throw error
+-- please ignore the error
+drop database pg2;
+
+-- Then create the database
+create database pg2;
+
+-- Create the table that stores the Paying Guest Table Data
+create table pg2.pg
+(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name char(20),
+addr varchar(30),
+last_updated_on timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAM,
+ created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+);
+
+-- Create the table that stores the Feature of the Paying Guest table, User can store as many as feature, a PG has
+-- but the name of feature and the value can't have more then 100 character. 
+-- this table refrences pg table using column pg_id
+create table pg2.features
+(
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ feature_name varchar(100),
+ value varchar(100),
+ last_updated_on timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAM,
+ created_on timestamp DEFAULT CURRENT_TIMESTAMP,
+ -- created_by = userid or username (db or app)
+ -- last_update_by = userid or username (db or app)
+ pg_id INT,
+ FOREIGN KEY (pg_id) 
+        REFERENCES pg(id)
+    
+);
+  
+  
+ 
+    
+
