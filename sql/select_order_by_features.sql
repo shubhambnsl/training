018a768b2bec889d643ac56f2d2da2131b4ba797
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-14 19:04:05 +0000

    Re-arrange the query which is now easily readable

--- sql/select_order_by_features.sql
+++ sql/select_order_by_features.sql
@@ -1,1 +1,3 @@
-select name as NAME,has_food + has_laundary + has_airconditioner + has_tv + has_internet + has_waterheater AS NumberOfFeatures from pg1.pg ORDER BY NumberOfFeatures DESC;
+select name as NAME,
+has_food + has_laundary + has_airconditioner + has_tv + has_internet + has_waterheater AS NumberOfFeatures 
+from pg1.pg ORDER BY NumberOfFeatures DESC;
