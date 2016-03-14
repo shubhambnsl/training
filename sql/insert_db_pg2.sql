c3b1f249f75f3244fdee0eefc336a460ad034ce7
shubhambnsl <shubham.bansal@targetintegration.com>
2016-03-14 12:03:58 +0000

    insert data in feature table of pg2 database

--- sql/insert_db_pg2.sql
+++ sql/insert_db_pg2.sql
@@ -1,1 +1,7 @@
-insert into pg2.pg(name,addr) VALUES ("xyz_pg","sector-28");
+insert into pg2.pg(id, name,addr) VALUES (1, "xyz_pg","sector-28");
+
+insert into pg2.features (feature_name, value,pg_id) VALUES ('has_Kitchen','0', 1);
+
+insert into pg2.features (feature_name, value,pg_id) VALUES ('has_airconditiong','1', 2);
+
+
